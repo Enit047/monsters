1cac163bb6b6c92482d0fbfeffe4da7982355076
Semyon <zen310011@gmail.com>
2020-04-14 17:03:57 +0300

    Updates

---
+++ precache-manifest.d39149f42150579704c11d2b70576d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578df2acc88c7eb1d7996c0d4fdde982",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "f2d310fb693f37e92543",
+    "url": "/monsters/static/css/main.96895f44.chunk.css"
+  },
+  {
+    "revision": "20b1912bf242704f9f11",
+    "url": "/monsters/static/js/2.0a6b9ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.0a6b9ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d310fb693f37e92543",
+    "url": "/monsters/static/js/main.0fd42b80.chunk.js"
+  },
+  {
+    "revision": "360bbdfb15409043b41d",
+    "url": "/monsters/static/js/runtime-main.6c4a148e.js"
+  }
+]);
